f6322ace118cad51ab5632111a8f1feaf5b7c800
JrSchild <joram.ruitenschild@gmail.com>
2015-11-04 22:43:39 +0100

    Even faster util.splitTime function.

--- benchmark/splitTime.js
+++ benchmark/splitTime.js
@@ -11,17 +11,20 @@ var options = [
 var now = Date.now();
 
 (new Benchmark.Suite)
-.add('Normal splitTime', () => {
-  splitTimeOld(now, options);
+.add('First splitTime', () => {
+  splitTimeFirst(now, options);
 })
-.add('Newer splitTime', () => {
+.add('Second splitTime', () => {
+  splitTimeSecond(now, options);
+})
+.add('NewerNewer splitTime', () => {
   utils.splitTime(now, options);
 })
 .on('cycle', (event) => console.log(`${event.target}`))
 .run({async: true});
 
 
-function splitTimeOld(time, options) {
+function splitTimeFirst(time, options) {
   var start = moment(time).startOf('hours');
 
   var result = {
@@ -50,3 +53,30 @@ function splitTimeOld(time, options) {
 
   return result;
 };
+
+function splitTimeSecond(time, options) {
+  var duration = time % 3600000;
+  var result = {
+    hours: time - duration
+  };
+  var index;
+
+  // Subtract each interval from the duration.
+  for (var i = 0, l = options.length; i < l; i++) {
+
+    // Current index is the biggest interval that fits inside the time rounded down.
+    index = ~~(duration / options[i][1]);
+    result[options[i][0]] = {
+      i: index,
+      v: index * options[i][1]
+    };
+
+    // Remove the current value from the duration for the next iteration.
+    duration -= result[options[i][0]].v
+  }
+
+  // Set the leftover time on the object.
+  result.milliseconds = duration;
+
+  return result;
+}
