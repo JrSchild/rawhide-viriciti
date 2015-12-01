ee41eb7455e726c9e93a46933725027fa4e6740b
Joram Ruitenschild <joram.ruitenschild@gmail.com>
2015-12-01 12:04:33 +0100

    Results to table: Fix sorting

--- transform/results-to-table.js
+++ transform/results-to-table.js
@@ -3,19 +3,19 @@ var _ = require('lodash');
 var bytes = require('bytes');
 var results = require('../results/results.aggregate.json');
 
-// Calculate lowest time.
-var min = _.min(results, (result) => result.time).time;
+// Calculate highest time.
+var max = _.max(results, (result) => result.time).time;
 
 var rows = Object.keys(results).map((name) => {
   var result = results[name];
 
-  result.speed = Math.round(result.time / min * 1000) / 10;
+  result.speed = Math.round(max / result.time * 1000) / 10;
   result.size = bytes(result.size);
   result.avgObjSize = bytes(result.avgObjSize);
 
-  return [name].concat(_.values(_.pick(result, 'time', 'size', 'avgObjSize', 'totalIndexSize', 'speed')));
-});
+  return [name].concat(_.values(_.pick(result, 'time', 'count', 'size', 'totalIndexSize', 'speed')));
+}).sort((a, b) => b[5] - a[5]);
 
-rows.unshift(['name', 'time (sec)', 'size', 'avgObjSize', 'indexSize', 'speed (%)']);
+rows.unshift(['name', 'time (sec)', 'count', 'size', 'indexSize', 'speed (%)']);
 rows = rows.map((row) => row.join(';')).join('\n');
 fs.writeFileSync('../results/results.aggregate.csv', rows);
