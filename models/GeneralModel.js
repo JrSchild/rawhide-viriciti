a0d814cb00c73698cf53f746e19a74f9482b8310
Joram Ruitenschild <joram.ruitenschild@gmail.com>
2015-12-02 14:09:28 +0100

    Create General Model that accepts CLI parameters to configure what variation and format to run. Create a better util.makeValues function that accepts an array format and and array of variation.

---
+++ models/GeneralModel.js
@@ -0,0 +1,81 @@
+'use strict'
+
+var BaseModel = require('./BaseModel');
+var utils = require('../lib/utils');
+var argv = require('yargs')
+  .default('variation', 'a')
+  .default('format', 3)
+  .check(function (argv) {
+    if (!/^[abcd]{1}$/.test(argv.variation)) {
+      throw 'Only variation a,b,c or d allowed.'
+    }
+    if (1 > argv.format || argv.format > 8) {
+      throw 'Format must be between 1 and 8.'
+    }
+    return true;
+  })
+  .argv;
+
+const ADAPTERS = {
+  MongoDB: 'MongoDBSimpleNestedMapAdapter'
+};
+
+const variations = {
+  a: [Object, Object],
+  b: [Object, Array],
+  c: [Array, Object],
+  d: [Array, Array]
+};
+const formats = [
+  [60000, 1000],
+  [3600000, 60000, 1000],
+  [3600000, 120000, 5000],
+  [3600000, 180000, 10000],
+  [3600000, 240000, 15000],
+  [3600000, 360000, 36000, 1000],
+  [3600000, 300000, 25000, 1000],
+  [3600000, 300000, 20000, 1000]
+];
+
+const format = formats[argv.format - 1];
+const variation = variations[argv.variation];
+const B = variation[0] === Object ? 1 : 0;
+
+class GeneralModel extends BaseModel {
+  WRITE(data, done) {
+    var times = utils.splitTimeArray(data.t, format);
+    var query = {
+      _id: times[0]
+    };
+    var update = {$set: {}};
+    var updateStr = 'values.';
+
+    var i = 1, l = format.length;
+    for (; i < l; i++) {
+      updateStr += `${times[i][B]}.values.`;
+    }
+    updateStr += `${times[l]}`;
+
+    update.$set[updateStr] = data.v;
+
+    this.adapter.UPDATE(this.parameters.thread.tableName, query, update, done);
+  }
+
+  READ(done) {
+    this.adapter.READ(done);
+  }
+
+  getDocumentModel() {
+    if (variation[0] === Object && variation[1] === Object)
+      return {};
+
+    return utils.makeValuesArray(format, variation);
+  }
+
+  static name() {
+    return `Model-1-${argv.format}-${argv.variation}`;
+  }
+}
+
+module.exports = GeneralModel;
+module.exports.ADAPTERS = ADAPTERS;
